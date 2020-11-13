696a0f49ab2cbaa2a16917227f5883ac1622132e
basharovi <hello@basharovi.tech>
2020-11-13 16:42:15 +0600

    Added Service Registration

---
+++ Dapper.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Application.Interfaces;
+using Dapper.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dapper.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
