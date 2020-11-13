367f086cb56d8d4be36ea1bef3cba5acfe41baec
basharovi <hello@basharovi.tech>
2020-11-13 16:47:19 +0600

    Added Product Controller

---
+++ Dapper.Api/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Dapper.Application.Interfaces;
+using Dapper.Core.Entities;
+
+namespace Dapper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _unitOfWork.Products.GetAllAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _unitOfWork.Products.GetByIdAsync(id);
+            if (data == null) return Ok();
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product)
+        {
+            var data = await _unitOfWork.Products.AddAsync(product);
+            return Ok(data);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var data = await _unitOfWork.Products.DeleteAsync(id);
+            return Ok(data);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Product product)
+        {
+            var data = await _unitOfWork.Products.UpdateAsync(product);
+            return Ok(data);
+        }
+    }
+}
--- Dapper.Api/Startup.cs
+++ Dapper.Api/Startup.cs
@@ -1,16 +1,10 @@
+using Dapper.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Dapper.Api
 {
@@ -26,11 +20,11 @@ namespace Dapper.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructure();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dapper.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dapper Crud App", Version = "v1" });
             });
         }
 
